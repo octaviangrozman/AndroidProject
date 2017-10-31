cc4ba0e62cea27ac71a330a3d376d24a7fff43b7
octaviangrozman <octaviangrozman@yahoo.com>
2017-10-31 11:13:54 +0100

    Logs added

--- app/src/main/java/com/example/octav/androidproject/MainActivity.java
+++ app/src/main/java/com/example/octav/androidproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.octav.androidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("asda", "2");
+        Log.i("asda", "2");
     }
 }
