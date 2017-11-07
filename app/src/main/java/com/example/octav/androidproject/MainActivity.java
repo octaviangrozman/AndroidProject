6ef6906d969bc21da88a7b28297e33b8be3f6fa1
octaviangrozman <octaviangrozman@yahoo.com>
2017-11-07 09:05:33 +0100

    Firebase set up

--- app/src/main/java/com/example/octav/androidproject/MainActivity.java
+++ app/src/main/java/com/example/octav/androidproject/MainActivity.java
@@ -3,17 +3,40 @@ package com.example.octav.androidproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("asda", "2");
-        Log.i("asda", "2");
-        Log.i("OCTAV MOLODETS", "2");
+        db = FirebaseDatabase.getInstance();
+        textView = (TextView) findViewById(R.id.title);
+        readData();
+    }
 
+    public void readData() {
+        db.getReference("fake").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String fakeData = dataSnapshot.getValue(String.class);
+                textView.setText(fakeData);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
