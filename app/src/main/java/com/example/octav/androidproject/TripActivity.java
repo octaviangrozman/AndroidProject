4057a8774c6712fccc7dde1ea27532be763e0f25
Nicolai <publicclubok@gmail.com>
2017-11-10 09:12:26 +0100

    Finish 'Trip Activity' activity layout and trip info display functionality

--- app/src/main/java/com/example/octav/androidproject/TripActivity.java
+++ app/src/main/java/com/example/octav/androidproject/TripActivity.java
@@ -10,36 +10,41 @@ import java.util.ArrayList;
 
 public class TripActivity extends AppCompatActivity {
 
-//    TextView titleLabel;
-    TextView titleTextView;
-//    TextView complexityLabel;
-//    TextView complexityTextView;
-//    TextView startPointLabel;
-//    TextView startPointTextView;
-//    TextView endPointLabel;
-//    TextView endPointTextView;
+    private TextView tripTitle;
+    private TextView tripDuration;
+    private TextView tripStartPoint;
+    private TextView tripEndPoint;
+    private TextView tripComplexity;
+    private TextView tripDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
 
-//        titleLabel = (TextView) findViewById(R.id.titleLabel);
-        titleTextView = (TextView) findViewById(R.id.title);
-//        complexityLabel = (TextView) findViewById(R.id.complexityLabel);
-//        complexityTextView = (TextView) findViewById(R.id.complexity);
-//        startPointLabel = (TextView) findViewById(R.id.startPointLabel);
-//        startPointTextView = (TextView) findViewById(R.id.startPoint);
-//        endPointLabel = (TextView) findViewById(R.id.endPointLabel);
-//        endPointTextView = (TextView) findViewById(R.id.endPoint);
+        initLayoutElements();
 
         Bundle bundle = getIntent().getExtras();
         Trip trip = (Trip)bundle.getSerializable("trip");
 
-        titleTextView.setText(trip.getTitle());
-//        complexityTextView.setText(String.valueOf(complexity));
-//        startPointTextView.setText(stops.get(0));
-//        endPointTextView.setText(stops.get(stops.size() - 1));
+        populateLayoutElements(trip);
+    }
 
+    private void populateLayoutElements(Trip trip) {
+        tripTitle.setText(trip.getTitle());
+        tripDuration.setText(String.valueOf(trip.getDuration()));
+        tripComplexity.setText(String.valueOf(trip.getComplexity()));
+        tripDescription.setText(trip.getDescription());
+        tripStartPoint.setText(trip.getStops().get(0));
+        tripEndPoint.setText(trip.getStops().get(trip.getStops().size() - 1));
+    }
+
+    private void initLayoutElements(){
+        tripTitle = (TextView) findViewById(R.id.title);
+        tripDuration = (TextView) findViewById(R.id.tripDuration);
+        tripStartPoint = (TextView) findViewById(R.id.tripStartPoint);
+        tripEndPoint = (TextView) findViewById(R.id.tripEndPoint);
+        tripDescription = (TextView) findViewById(R.id.tripDescription);
+        tripComplexity = (TextView) findViewById(R.id.tripComplexity);
     }
 }
